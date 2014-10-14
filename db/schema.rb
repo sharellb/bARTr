ade6db218fd3a0f56a92d165ce02a7fce66039b4
Sharell Bryant <sharell.bryant@gmail.com>
2014-10-14 13:54:09 -0400

    Fixing layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014144823) do
+ActiveRecord::Schema.define(version: 20141014173440) do
 
   create_table "conversations", force: true do |t|
     t.datetime "created_at"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20141014144823) do
   create_table "participants", force: true do |t|
     t.integer  "conversation_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
