846e9268db9f28854f619f0446e5ec19553deb62
Sharell Bryant <sharell.bryant@gmail.com>
2014-10-14 16:24:40 -0400

    Update edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,7 +22,7 @@ class ProfilesController < ApplicationController
 	def update
 		@user = User.find_by_profile_name(params[:id])
 		if @user.update(user_params)
-			render 'show'
+			render action: :show
 		else
 			render 'edit'
 		end
